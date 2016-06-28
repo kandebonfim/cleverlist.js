20667720b64ffeaed4298a67078b049ad670e67a
Kande Bonfim <kandebonfim@gmail.com>
2016-06-28 01:08:22 -0300

    Basic toSentence method

--- dist/cleverlist.js
+++ dist/cleverlist.js
@@ -1,4 +1,28 @@
 (function() {
-  console.log('test');
+  this.CleverList = (function() {
+    function CleverList(content, limit1) {
+      this.content = content;
+      this.limit = limit1 != null ? limit1 : 3;
+      console.log((this.toSentence(this.content, this.limit)) + " liked your foto.");
+    }
+
+    CleverList.prototype.toSentence = function(arr, limit) {
+      var last;
+      if (arr.length > limit + 1 && limit) {
+        last = (arr.length - limit) + " others";
+        arr = arr.splice(0, limit);
+      } else {
+        last = arr.pop();
+      }
+      if (!arr.length) {
+        return last;
+      } else {
+        return (arr.join(', ')) + " and " + last;
+      }
+    };
+
+    return CleverList;
+
+  })();
 
 }).call(this);
