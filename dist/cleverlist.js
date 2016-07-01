6e8af8113f56869bd70d3d243f22e5cbb1af0378
Kande Bonfim <kandebonfim@gmail.com>
2016-07-01 12:54:25 -0300

    Add template feature

--- dist/cleverlist.js
+++ dist/cleverlist.js
@@ -1,28 +1,55 @@
 (function() {
   this.CleverList = (function() {
-    function CleverList(content, limit1) {
-      this.content = content;
-      this.limit = limit1 != null ? limit1 : 3;
-      this.result = (this.toSentence(this.content, this.limit)) + " liked your foto.";
+    function CleverList(content1) {
+      this.content = content1;
+      this.tpl = {};
+      this.def = {
+        append: '.',
+        last: ' and ',
+        others: ' others',
+        comma: ', ',
+        limit: 3,
+        type: 'string'
+      };
     }
 
-    CleverList.prototype.toSentence = function(arr, limit) {
+    CleverList.prototype.toSentence = function(arr) {
       var last;
-      if (arr.length > limit + 1 && limit) {
-        last = (arr.length - limit) + " others";
-        arr = arr.splice(0, limit);
+      if (arr.length > this.def.limit + 1) {
+        last = "" + (arr.length - this.def.limit) + this.def.others;
+        arr = arr.splice(0, this.def.limit);
       } else {
         last = arr.pop();
       }
       if (!arr.length) {
         return last;
       } else {
-        return (arr.join(', ')) + " and " + last;
+        return "" + (arr.join(this.def.comma)) + this.def.last + last;
       }
     };
 
     CleverList.prototype.result = function() {
-      return this.result;
+      return "" + (this.toSentence(this.content)) + this.def.append;
+    };
+
+    CleverList.prototype.addTemplate = function(template, settings) {
+      return this.tpl[template] = settings;
+    };
+
+    CleverList.prototype.withTemplate = function(template, content) {
+      var currentTemplate, k;
+      this.content = content;
+      if (this.tpl[template]) {
+        currentTemplate = this.tpl[template];
+      } else {
+        console.error('Template not found.');
+      }
+      for (k in this.def) {
+        if (currentTemplate[k]) {
+          this.def[k] = currentTemplate[k];
+        }
+      }
+      return this.result();
     };
 
     return CleverList;
--- src/cleverlist.coffee
+++ src/cleverlist.coffee
@@ -1,10 +1,27 @@
 class @CleverList
-  constructor: (@content, @limit = 3) ->
-    @result = "#{@toSentence(@content, @limit)} liked your foto."
+  constructor: (@content) ->
+    @tpl = {} # templates
+    @def =  # default settings
+      append: '.'
+      last: ' and '
+      others: ' others'
+      comma: ', '
+      limit: 3
+      type: 'string'
 
-  toSentence: (arr, limit) ->
-    if arr.length > limit+1 and limit then last = "#{arr.length-limit} others"; arr = arr.splice(0,limit) else last = arr.pop()
-    unless arr.length then last else "#{arr.join(', ')} and #{last}"
+  toSentence: (arr) ->
+    if arr.length > @def.limit+1 then last = "#{arr.length-@def.limit}#{@def.others}"; arr = arr.splice(0,@def.limit) else last = arr.pop()
+    unless arr.length then last else "#{arr.join(@def.comma)}#{@def.last}#{last}"
 
   result: ->
-    return @result
+    return "#{@toSentence(@content)}#{@def.append}"
+
+  addTemplate: (template, settings) ->
+    @tpl[template] = settings
+
+  withTemplate: (template, content) ->
+    @content = content
+    if @tpl[template] then currentTemplate = @tpl[template] else console.error 'Template not found.'
+    for k of @def
+      if currentTemplate[k] then @def[k] = currentTemplate[k]
+    @result()
